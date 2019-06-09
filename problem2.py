468fa18b5a7dbf468e5a68faacb2c1e94c823a63
sk4201 <16tec2cs024@vgu.ac.in>
2019-06-09 21:50:49 +0530

    google search and store in the list

--- problem2.py
+++ problem2.py
@@ -1,3 +1,5 @@
+#write a code using  that will take user input from and search on google and store top 10 url in the list.
+
 import time
 from googlesearch import search
 web=input("please enter some topic:-")
