48d2adac1530a81e5690f6d16d40721fed2c0b51
sk4201 <16tec2cs024@vgu.ac.in>
2019-06-11 20:53:07 +0530

    year of the person in the age of 95

--- problem1.py
+++ problem1.py
@@ -5,4 +5,4 @@ import datetime
 name=input("Enter Your Name :")
 age=int(input("Enter your age :"))
 now = datetime.datetime.now()
-print(name," Will turn 95 in ",(95-age)+now.year)
+print(name," in the age of 95 year wiil be ",(95-age)+now.year)
