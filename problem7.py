bcac7e1dab525917252d5b9be559dc1199de9eac
sk4201 <16tec2cs024@vgu.ac.in>
2019-06-27 21:14:12 +0530

    question inside

---
+++ problem7.py
@@ -0,0 +1,35 @@
+
+'''
+question
+Problem 7: 
+
+use deep analysis of file handling to create a linux command similar to touch
+explore atleast 5 features of touch command
+
+Important :    explore touch command first then try to write code
+'''
+#Creating an new file
+location=input('Enter the location of file')
+file = open(location,"w")
+file.close()
+
+#Creating multiple file together
+num=int(input("Enter Number of new files to create"))
+for i in range(num):
+    location=input("Enter name of file ")
+    myfile=open(location,"w")
+myfile.close()
+
+#To check the access and modification time and update it
+import os
+location=input('Enter the location of file')
+stinfo = os.stat(location)
+print(stinfo)
+
+print(os.utime(location,(stinfo.st_atime,stinfo.st_mtime)))
+
+# upadate the modification time of one file with the other file
+loc1=input("Enter File path from which upadated date and time is extracted")
+loc2=input("Enter File path To Update date and time")
+
+os.utime(loc2(os.path.getmtime(loc1),os.path.getmtime(loc2)))
